bd922fcaca894790722ac61a5c56d08c31bb8fe8
mikseros <mmadynski@gmail.com>
2022-04-09 19:31:56 +0200

    "Hard-coding" of product one. Without controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', function() {
-    return 'products';
+    return Product::all();
+});
+
+Route::post('/products', function() {
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
